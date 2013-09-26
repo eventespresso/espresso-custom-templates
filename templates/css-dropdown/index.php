32d72ef71212e14e5687fed3e3fffd8e00077201
Seth <seth@eventespresso.com>
2013-09-26 13:55:26 -0600

    Adding Dean's jquery changes to the CSS drop down template. [touch:3602]

--- templates/css-dropdown/index.php
+++ templates/css-dropdown/index.php
@@ -1,3 +1,24 @@
+<script type="application/javascript">
+
+jQuery(document).ready(function(){
+								
+	//if javascript enabled disable CSS hover effect.
+	jQuery('.dd_js').removeClass('dd_js');
+	jQuery('#espresso-select li ul').css('display', 'none');
+	
+	
+	jQuery('#espresso-select').click(function() {									  
+		jQuery('#espresso-select ul').toggle();
+	});
+
+
+});
+
+
+</script>
+
+
+
 <?php 
 //Template Name: CSS Dropdown
 //Shortcode Example: [EVENT_CUSTOM_VIEW template_name="css-dropdown"]
@@ -7,6 +28,7 @@ add_action('action_hook_espresso_custom_template_css-dropdown','espresso_css_dro
 if (!function_exists('espresso_css_dropdown')) {
 	function espresso_css_dropdown(){	
 		global $events;
+	
 		
 		//Register styles
 		wp_register_style( 'espresso_css_dropdown', ESPRESSO_CUSTOM_DISPLAY_PLUGINPATH."templates/css-dropdown/style.css" );
@@ -19,24 +41,26 @@ if (!function_exists('espresso_css_dropdown')) {
 		wp_enqueue_style( 'fontAwesome');	
 ?>
 
-<ul id="espresso-select">
-	<li>
-		<h3><a href="#">
-			<?php _e('Upcoming Events', 'event_espresso'); ?>
-			</a></h3>
-		<i class="icon-chevron-sign-down"></i>
-		<ul>
-			<?php 
-		foreach ($events as $event){ 
-			$externalURL = $event->externalURL; $registration_url = !empty($externalURL) ? $externalURL : espresso_reg_url($event->id);?>
-			<li><a class="a_event_title" id="a_event_title-<?php echo $event->id; ?>" href="<?php echo $registration_url; ?>"><?php echo stripslashes_deep($event->event_name)?><br />
-				<?php echo event_date_display($event->start_date, 'M j, Y'); ?></a> </li>
-			<?php 
-		}
-		?>
-		</ul>
-	</li>
-</ul>
+<div>
+    <ul id="espresso-select" class="dd_js">
+        <li>
+            <h3><a href="#">
+                <?php _e('Upcoming Events', 'event_espresso'); ?>
+                </a></h3>
+            <i class="icon-chevron-sign-down"></i>
+            <ul>
+                <?php 
+            foreach ($events as $event){ 
+                $externalURL = $event->externalURL; $registration_url = !empty($externalURL) ? $externalURL : espresso_reg_url($event->id);?>
+                <li><a class="a_event_title" id="a_event_title-<?php echo $event->id; ?>" href="<?php echo $registration_url; ?>"><?php echo stripslashes_deep($event->event_name)?><br />
+                    <?php echo event_date_display($event->start_date, 'M j, Y'); ?></a> </li>
+                <?php 
+            }
+            ?>
+            </ul>
+        </li>
+    </ul>
+</div>
 <?php
 	}
 }
