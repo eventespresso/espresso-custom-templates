4bd1145ba347d3d5c657ef1f55a3616a2ee06dc1
Dean Robinson <info@apinapress.com>
2013-10-02 20:37:26 +0300

    Added another template - Category Accordion. Lists the categories, clicking them reveals the associated events.

--- templates/category-filter/index.php
+++ templates/category-filter/index.php
@@ -1,30 +1,30 @@
-<?php 
+<?php
 
 //Template: Category Filter
 //Description: This template creates a list of events, displayed in a table. It can display events by category and/or maximum number of days.  The table has a javascript filtering system to allow the user to filter the list by category.
 //Shortcode Example: [EVENT_CUSTOM_VIEW template_name="category-filter"]
 //Requirements: CSS skills to customize styles, some renaming of the table columns
 
-//The end of the action name (example: "action_hook_espresso_custom_template_") should match the name of the template. In this example, the last part the action name is "default", 
+//The end of the action name (example: "action_hook_espresso_custom_template_") should match the name of the template. In this example, the last part the action name is "default",
 add_action('action_hook_espresso_custom_template_category-filter','espresso_custom_template_category_filter');
 
 function espresso_custom_template_category_filter(){
 
 	global $this_event_id, $events, $wpdb;
-	
+
 	//Check for Multi Event Registration
 	$multi_reg = false;
 	if (function_exists('event_espresso_multi_reg_init')) {
 		$multi_reg = true;
 	}
-	
+
 	$cart_link 	= '';
-	
+
 	$sql = "SELECT * FROM " . EVENTS_CATEGORY_TABLE;
 	$temp_cats = $wpdb->get_results($sql);
 
 ?>
-    
+
 
 <label><?php echo __('Filter by category ', 'event_espresso'); ?></label>
 
@@ -36,9 +36,9 @@ function espresso_custom_template_category_filter(){
 		}
     ?>
     </select>
-    
+
 	<table id="ee_filter_table" class="espresso-table" width="100%">
-  
+
       <thead class="espresso-table-header-row">
       <tr>
           <th class="th-group"><?php _e('Course','event_espresso'); ?></th>
@@ -53,7 +53,7 @@ function espresso_custom_template_category_filter(){
 	<tbody>
 
       <?php
-      
+
       foreach ($events as $event){
 		$button_text 		= __('Register', 'event_espresso');
 		$alt_button_text	= __('View Details', 'event_espresso');//For alternate registration pages
@@ -62,8 +62,8 @@ function espresso_custom_template_category_filter(){
 		$registration_url 	= !empty($externalURL) ? $externalURL : espresso_reg_url($event->id);
 		$open_spots			= apply_filters('filter_hook_espresso_get_num_available_spaces', $event->id);
 		$live_button 		= $open_spots < 1 ? __('Closed', 'event_espresso') : '<a id="a_register_link-'.$event->id.'" href="'.$registration_url.'">'.$button_text.'</a>';
-		
-		if ($multi_reg && event_espresso_get_status($event->id) == 'ACTIVE' && empty($externalURL)) { 
+
+		if ($multi_reg && event_espresso_get_status($event->id) == 'ACTIVE' && empty($externalURL)) {
 			$params = array(
 				//REQUIRED, the id of the event that needs to be added to the cart
 				'event_id' => $event->id,
@@ -78,13 +78,13 @@ function espresso_custom_template_category_filter(){
 
 			$cart_link = event_espresso_cart_link($params);
 		}
-		
-		
+
+
 	   ?>
       <tr class="espresso-table-row cat-<?php echo $event->category_id; ?>">
        	<td class="td-group">
             <?php echo stripslashes_deep($event->event_name) ?>
-          </td>	
+          </td>
           <td class="td-group">
             <?php echo $event->venue_address ?>
           </td>
@@ -100,7 +100,7 @@ function espresso_custom_template_category_filter(){
           <td class="td-group">
               <?php echo espresso_event_time($event->id, 'start_time', get_option('time_format')) ?>
           </td>
-         
+
           <td class="td-group">
               <?php echo $live_button. $cart_link ?>
           </td>
@@ -112,7 +112,7 @@ function espresso_custom_template_category_filter(){
 
 jQuery(document).ready(function(){
 
-		jQuery("#ee_filter_cat").change(function() { 
+		jQuery("#ee_filter_cat").change(function() {
 				var ee_filter_cat_id = jQuery("option:selected").attr('class');
 				console.log(ee_filter_cat_id);
 				jQuery("#ee_filter_table .espresso-table-row").show();
