4a69d3a83e953e846396ee28d4b155568a91f027
Seth <seth@eventespresso.com>
2013-10-24 23:53:17 -0600

    Adding a css class

--- templates/events-table/index.php
+++ templates/events-table/index.php
@@ -91,7 +91,7 @@ function espresso_custom_template_events_table(){
 			<td id="event_title-<?php echo $event->id?>" class="event_title"><?php echo stripslashes_deep($event->event_name) ?></td>
 			<td id="venue_title-<?php echo $event->id?>" class="venue_title"><?php echo $event->venue_name ?></td>
 			<td id="start_date-<?php echo $event->id?>" class="start_date"><?php echo event_date_display($event->start_date.' '.$event->start_time, get_option('date_format').' '.get_option('time_format')) ?></td>
-			<td class="td-group"><?php echo $event_status == 'ACTIVE' ? $live_button .  $cart_link : $live_button; ?></td>
+			<td class="td-group reg-col" nowrap="nowrap"><?php echo $event_status == 'ACTIVE' ? $live_button .  $cart_link : $live_button; ?></td>
 		</tr>
 		<?php } //close foreach ?>
 	</tbody>
