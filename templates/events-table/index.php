086bdd7b95d00b1317c11cc8e75eda75737ef044
Tony Warwick <tony@eventespresso.com>
2014-07-22 12:08:03 +0100

    Added a check for the new template function. Not strictly needed in this case, but doesn't hurt. [touch: 4532]

--- templates/events-table/index.php
+++ templates/events-table/index.php
@@ -7,20 +7,24 @@
 //The end of the action name (example: "action_hook_espresso_custom_template_") should match the name of the template. In this example, the last part the action name is "default",
 add_action('action_hook_espresso_custom_template_events-table','espresso_custom_template_events_table');
 
-function espresso_custom_template_events_table(){
+function espresso_custom_template_events_table() {
 
 	global $this_event_id, $events, $wpdb, $ee_attributes;
 
 	extract($ee_attributes);
-	
+
 	//Get the categories
 	$sql = "SELECT * FROM " . EVENTS_CATEGORY_TABLE;
 	$temp_cats = $wpdb->get_results($sql);
-	
+
 	//Check for custom templates
-	$custom_template_path = espresso_custom_template_locate("events-table");
-	
-	if( !empty($custom_template_path) ){
+	if(function_exists('espresso_custom_template_locate')) {
+		$custom_template_path = espresso_custom_template_locate("events-table");
+	} else {
+		$custom_template_path = '';
+	}
+
+	if( !empty($custom_template_path) ) {
 		//If custom template found include here
 		include( $custom_template_path );
 	} else {
