72d6d96213ac83d232942b3a84dd57b1ee98ce7b
Josh Feck <josh@nxnweb.com>
2019-12-03 23:16:23 -0500

    avoid fatal if no array

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ function espresso_custom_template_locate($template_name){
 
 add_action('action_hook_espresso_custom_template_output', 'espresso_custom_template_display', 10, 1 );
 //HTML to show the events on your page in matching table. To customize this layout, please copy and paste the following code into your theme/functions.php file.
-function espresso_custom_template_display(array $attributes) {
+function espresso_custom_template_display($attributes) {
 
 	if( !defined('ESPRESSO_CUSTOM_DISPLAY_PLUGINPATH')){
 		define("ESPRESSO_CUSTOM_DISPLAY_PLUGINPATH", WP_PLUGIN_URL. "/".plugin_basename(dirname(__FILE__)) . "/");
@@ -94,10 +94,14 @@ function espresso_custom_template_display(array $attributes) {
 
 	);
 
-	$attributes = array_merge(
-		$default_attributes,
-		$attributes
-	);
+	if(!is_array($attributes)) {
+		$attributes = $default_attributes;
+	} else {
+		$attributes = array_merge(
+			$default_attributes,
+			$attributes
+		);
+	}
 
 	//Create a global to hold the shortcode attributes/parameters
 	global $ee_attributes;
